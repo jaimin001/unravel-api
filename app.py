409594840dad25a7a6af61ea2b89b111dbf85c76
jaimin <jaimingajjar01@gmail.com>
2023-06-16 20:32:44 +0530

    constructed the app pipeline

--- app.py
+++ app.py
@@ -1,41 +1,75 @@
+import os
 import streamlit as st
+from utils import create_database_for_link, load_documents
+from dotenv import load_dotenv
+import asyncio
+import nest_asyncio
 
-# Page layout and title
-st.set_page_config(page_title='UnRavel APIs', layout='wide')
-st.title('UnRavel APIs')
 
-# Input section
-st.header('Enter the API Documentation Links')
-api_links = st.text_area('API Documentation Links (one link per line)')
+def main():
+    asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
+    # nest_asyncio.apply()
+    load_dotenv()
+    # Set the OpenAI API key
+    os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 
-# Process and display output
-if st.button('Understand the API(s)'):
-    if not api_links:
-        st.warning('Please enter the API documentation links.')
-    else:
-        # Split the input into separate links
-        api_links_list = api_links.split('\n')
-        # Remove empty elements
-        api_links_list = [link.strip() for link in api_links_list if link.strip()]
-        
-        if len(api_links_list) == 0:
-            st.warning('Please enter valid API documentation links.')
+    # Page layout and title
+    st.set_page_config(page_title='UnRavel APIs', layout='wide')
+    st.title('UnRavel APIs')
+
+    # Input section
+    st.header('Enter the API Documentation Links')
+    api_links = st.text_area('API Documentation Links (one link per line)')
+
+    # Process and display output
+    if st.button('Understand the API(s)'):
+        if not api_links:
+            st.warning('Please enter the API documentation links.')
         else:
-            st.info('Processing the API documentation...')
-            # TODO: Replace with processing logic for the API documentations
-            st.success('All the API(s) are Understood!')
+            # Split the input into separate links
+            api_links_list = api_links.split('\n')
+            # Remove empty elements
+            api_links_list = [link.strip() for link in api_links_list if link.strip()]
+            
+            if len(api_links_list) == 0:
+                st.warning('Please enter valid API documentation links.')
+            else:
+                st.info('Processing the API documentation...')
+                # TODO: Replace with processing logic for the API documentations
+                for i, link in enumerate(api_links_list):
+                    st.info(f'Processing {link}')
+                    
+                    path = f"data/data_{i}"
+                    if not os.path.exists(path):
+                        os.makedirs(path)
+                        os.makedirs(path + "/files")
+                    
+                    max_attempts = int(os.getenv('MAX_ATTEMPTS', 5)) 
+                    succ_diff = create_database_for_link(link, path, max_attempts=max_attempts)
+                    if succ_diff:
+                        st.success(f'API {i+1} is Understood!')
+                    else:
+                        st.error(f'Some error occured while understanding API {i+1}!')
+                    
+                    docs = load_documents(path + "/files")
+                    
+                st.success('All the API(s) are Understood!')
 
-            # Chat interface
-            if st.button('Start Chatting'):
-                st.subheader('Chat Interface')
-                user_query = st.text_input('Enter your query')
+                # Chat interface
+                if st.button('Start Chatting'):
+                    st.subheader('Chat Interface')
+                    user_query = st.text_input('Enter your query')
 
-                if user_query:
-                    # TODO: Replace with search logic
-                    # TODO: Display search results
-                    pass
+                    if user_query:
+                        # TODO: Replace with search logic
+                        # TODO: Display search results
+                        pass
 
 
-# Footer
-st.markdown('---')
-st.markdown('<a href="https://www.buymeacoffee.com/jaiminsg" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me a Coffee" width="150" height="40"></a>', unsafe_allow_html=True)
+    # Footer
+    st.markdown('---')
+    st.markdown('<a href="https://www.buymeacoffee.com/jaiminsg" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me a Coffee" width="150" height="40"></a>', unsafe_allow_html=True)
+
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -2,16 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urldefrag
 from langchain.document_loaders import DirectoryLoader
-import nest_asyncio
 from langchain.document_loaders import WebBaseLoader
-nest_asyncio.apply()
 import os
 import asyncio
 import aiohttp
 import tiktoken
+import time
 
 def extract_unique_absolute_urls(url, visited_links=None, depth_limit=3, current_depth=0):
-# all_unique_absolute_urls = extract_unique_absolute_urls(website_url, depth_limit=depth_limit)
     if visited_links is None:
         visited_links = set()
 
@@ -37,11 +35,17 @@ def extract_unique_absolute_urls(url, visited_links=None, depth_limit=3, current
     return visited_links
 
 
+def save_list_to_file(data_list, file_path):
+    with open(file_path, 'w') as file:
+        for item in data_list:
+            file.write(str(item) + '\n')
+
+
 async def download_html_page(session, url, folder):
     try:
         async with session.get(url) as response:
             response.raise_for_status()
-            html_content = await response.text()
+            html_content_bytes = await response.content.read()
 
             # Extract the file name from the URL
             filename = url.split("/")[-1] + ".html"
@@ -51,12 +55,16 @@ async def download_html_page(session, url, folder):
 
             # Save the HTML content to a file in the specified folder
             filepath = os.path.join(folder, filename)
-            with open(filepath, "w", encoding="utf-8-sig") as file:
-                file.write(html_content)
+            try:
+                with open(filepath, "w", encoding="utf-8-sig") as file:
+                    file.write(html_content_bytes)
+            except:
+                pass
 
             print(f"Successfully downloaded HTML from {url} and saved as {filepath}")
     except aiohttp.ClientError as e:
         print(f"Error downloading HTML from {url}: {str(e)}")
+
 
 async def download_html_pages(url_list, folder):
     async with aiohttp.ClientSession() as session:
@@ -66,11 +74,6 @@ async def download_html_pages(url_list, folder):
             tasks.append(task)
 
         await asyncio.gather(*tasks)
-
-## How to use:
-# folder_path = "./downloads/"
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(download_html_pages(all_unique_absolute_urls, folder_path))
 
 def load_documents(folder_path):
     loader = DirectoryLoader(folder_path, glob="**/*.html")
@@ -86,3 +89,35 @@ def load_documents(folder_path):
 # tokenizer = tiktoken.get_encoding('cl100k_base')
 # def tiktoken_length(text):
 #     return len(tokenizer.encode(text))
+
+
+def create_database_for_link(api_doc_url, path, max_attempts=3):
+    
+    success = False
+    for attempt in range(1, max_attempts+1):
+        try:
+            print('Scraping the Documentation...\n')
+            start_time = time.time() 
+             
+            ## Get all the absolute urls
+            print(api_doc_url)
+            absolute_urls = extract_unique_absolute_urls(url=api_doc_url, depth_limit=1)
+            ## save the absolute urls to a file
+            path_to_absolute_urls = f'{path}/absolute_urls.txt'
+            save_list_to_file(absolute_urls, path_to_absolute_urls)
+            ## Download all the corresponding pages
+            path_to_html_pages = f'{path}/files'
+            asyncio.run(download_html_pages(absolute_urls, path_to_html_pages))
+            
+            elapsed_time = time.time() - start_time  
+            print(f"Time spent to pull the data: {elapsed_time:.2f}s.")
+            success = True
+            break  
+        except Exception as e:
+            print(f"Attempt {attempt} failed with error: {e}")
+            if attempt == max_attempts:
+                print("Max attempts reached. Exiting...")
+                return
+            else:
+                print("Retrying...")
+    return success
