a84266b5536688dcfd3b6f48b7f3f64bbedc807a
jaimin <jaimingajjar01@gmail.com>
2023-06-26 23:04:51 +0530

    updated app.py with backend code

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 import os
 from dotenv import load_dotenv
+from utils import create_database_for_link, load_documents, split_documents
 
 
 load_dotenv()
-
-st.title('UnRavel APIs')
 
 # Set up DeepLake vector store
 dataset_path = os.getenv('DATASET_PATH')
@@ -27,6 +26,45 @@ def generate_response(input_text):
     st.info(response)
 
 chat_history = []
+
+###########
+st.set_page_config(page_title='UnRavel APIs', layout='wide')
+st.title('UnRavel APIs')
+
+st.header('Enter the API Documentation Links')
+api_link = st.text_area('API Documentation Link')
+
+if st.button('Understand the API(s)'):
+    if not api_link:
+        st.warning('Please enter the API documentation links.')
+    else:
+        api_link = api_link.strip()
+        
+        st.info('Processing the API documentation...')
+        st.info(f'Processing {api_link}')
+        
+        path = f"data/data"
+        if not os.path.exists(path):
+            os.makedirs(path)
+            os.makedirs(path + "/files")
+        
+        max_attempts = int(os.getenv('MAX_ATTEMPTS', 5)) 
+        succ_diff = create_database_for_link(api_link, path, max_attempts=max_attempts)
+        
+        docs = load_documents(path + "/files")
+        texts = split_documents(docs)
+        
+        dataset_path = os.getenv('DATASET_PATH')
+        db = DeepLake(dataset_path=dataset_path, embedding_function=OpenAIEmbeddings(), overwrite=True)
+        db.add_documents(texts)
+        print('Vector database updated.')
+        
+        if succ_diff or texts == False:
+            st.success(f'API is Understood!')
+        else:
+            st.error(f'Some error occured while understanding API!')
+            
+###########
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'Ask a question or say something...')
